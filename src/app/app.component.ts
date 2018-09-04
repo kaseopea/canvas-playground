4ae9432e1181770f6d0e456029a9deb5c74513ed
Vitali.Rusau <Vitali_Rusau@epam.com>
2018-09-04 10:32:21 +0300

    Canvas Adventures Lint fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent implements AfterViewInit {
   @ViewChild('playground') canvasEl: ElementRef;
 
   ngAfterViewInit() {
     const canvas = this.canvasEl.nativeElement;
+
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
   }
